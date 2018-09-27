409f8a65caa9032be931cd7581cb303a97a27f78
tharshana <tharshanathiruchselvanathan@gmail.com>
2018-09-27 16:53:08 +0530

    upd

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
   'facebookAuth' : {
     'clientID'      : '331701414245397', // your App ID
     'clientSecret'  : 'b35b76ffdf58dfe5cd53dd96efa0661c', // your App Secret
-    'callbackURL'   : 'https://assignment3bytharshana.herokuapp.com/auth/facebook/callback',
+    'callbackURL'   : 'https://assignment3tharshana.herokuapp.com/auth/facebook/callback',
     'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
     'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
   },
@@ -12,7 +12,7 @@ module.exports = {
   'googleAuth' : {
     'clientID'      : '372891224663-596er1ecclrbtkaou2jsagt6bo40iglp.apps.googleusercontent.com',
     'clientSecret'  : 'OlDZr1lXqrQ_nTStmT4EKPH7',
-    'callbackURL'   : 'https://assignment3bytharshana.herokuapp.com/auth/google/callback'
+    'callbackURL'   : 'https://assignment3tharshana.herokuapp.com/auth/google/callback'
   }
 
 };
