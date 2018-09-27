314fa63efcc4e4152eb0aebd1dc33e74aea09cba
tharshana <tharshanathiruchselvanathan@gmail.com>
2018-09-27 08:28:47 +0530

    assignment3

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-     'url' : 'mongodb://localhost:27017/auth'
-      // 'url' : 'mongodb://tharshana:tharsh4@ds113923.mlab.com:13923/assignment3'
+     // 'url' : 'mongodb://localhost:27017/auth'
+      'url' : 'mongodb://tharshana:tharsh4@ds113923.mlab.com:13923/assignment3'
 
 };
